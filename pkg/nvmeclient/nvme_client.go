LightBitsLabs/discovery-client

commit 3b7d7def9e6fdd2bd3456f6f47deec64f712d507

nvme/cfg: eliminate stale ctrls by using a finite ctrl_loss_tmo
Until now we had a ctrl_loss_tmo set to -1 by default.
This is likely a wrong lesson learnt from systems and tests
with "manual" nvme connect's, where indeed ctrl_loss_tmo set
lower than the maximal node downtime results in missing
node connection later when the node comes back.

This means a controller, once connected, is never removed,
e.g. if a server is taken down/deleted,
the corresponding controllers will remain
reconnecting forever on all client machines,
until manually disconnected.

Solution:
- Add ctrlLossTMO to discovery-client.yaml
  (int, seconds, default is 600)
- Thus, any controller that cannot reconnect within this time
  will be automatically removed by nvme subsystem.
NOTE: this won't take effect for existing controllers
  connected by an older discovery-client.
  These can be adjusted by
  echo 600 > /sys/class/nvme/nvme0/ctrl_loss_tmo

Consequences to expect:
1) If an server node is down and the corresponding controller
is removed after 600s, discovery-client will reconnect it once
the node becomes active: the cluster discovery service notifies
relevant discovery-clients when a node becomes active.
2) If a client looses network connectivity to the servers,
and the IO controllers are removed after 600s,
But, discovery-client will also get disconnected and will keep
trying to reconnect to the discovery endpoints.
When network connectivity is restored, DC will succeed, fetch a log page,
and connect all the required IO controllers.
3) If a server is removed from the cluster, the discovery client configuration
will still have this endpoint listed: discovery-client attempts to connect to it
will fail to connect to a server that is gone or down, thus another
endpoint will be picked.
4) If a client looses connection to all replicas of a volume
(due to LB nodes being down or a network issue like (2),
after 600s all IO controllers are removed, and the IO to the volume will fail

pkg/nvmeclient/nvme_client.go
@@ -620,7 +620,7 @@ func ConnectAllNVMEDevices(logPageEntries []*hostapi.NvmeDiscPageEntry,
 		if logPageEntry.SubType != nvme.NVME_NQN_NVME {
 			continue
 		}
-		ctrlLossTMOValue := -1
+		ctrlLossTMOValue := cfg.CtrlLossTMO
 		if ctrlLossTMO != nil {
 			ctrlLossTMOValue = *ctrlLossTMO
 		}

model/app_config.go
@@ -57,6 +57,7 @@ type AppConfig struct {
 	AuxSuffix                string            `yaml:"auxSuffix,omitempty"`
 	DhChapSecret             string            `yaml:"dhChapSecret,omitempty"`
 	DhChapCtrlSecret         string            `yaml:"dhChapCtrlSecret,omitempty"`
+	CtrlLossTMO              int               `yaml:"ctrlLossTMO"`
 }
 
 func (cfg *AppConfig) verifyConfigurationIsValid() error {
@@ -71,6 +72,9 @@ func (cfg *AppConfig) verifyConfigurationIsValid() error {
 		return fmt.Errorf("dhchapsecret is mandatory when using dhchapctrlsecret")
 	}
 
+	if cfg.CtrlLossTMO == 0 || cfg.CtrlLossTMO < -1 {
+		cfg.CtrlLossTMO = 600
+	}
 	return cfg.Logging.IsValid()
 }
 

commit c965b6c8d067c88e690b679c87e93ac26584d84e

nvme_client: fix nil pointer dereference in connect-all
The commit below introduced an issue with command-line
operations like connect-all:
unlike IO ctrl connects initiated by discovery-client.service
command line connects do not have cfg set
causing a panic (NPD) on access to a nil cfg.

Fix:
- lossTMO default is -1
- the ctrlLossTMO argument, if provided, overrides
- otherwise the cfg.CtrlLossTMO, if cfg provided, overrides

Fixes: 3b7d7def9e6fdd2bd3456f6f47deec64f712d507

Signed-off-by: Anton Eidelman <anton@lightbitslabs.com>

pkg/nvmeclient/nvme_client.go
@@ -620,10 +620,14 @@ func ConnectAllNVMEDevices(logPageEntries []*hostapi.NvmeDiscPageEntry,
 		if logPageEntry.SubType != nvme.NVME_NQN_NVME {
 			continue
 		}
-		ctrlLossTMOValue := cfg.CtrlLossTMO
+
+		ctrlLossTMOValue := -1
 		if ctrlLossTMO != nil {
 			ctrlLossTMOValue = *ctrlLossTMO
+		} else if cfg != nil {
+			ctrlLossTMOValue = cfg.CtrlLossTMO
 		}
+
 		request := &ConnectRequest{
 			Traddr:      logPageEntry.Traddr,
 			Trsvcid:     int(logPageEntry.TrsvcID),